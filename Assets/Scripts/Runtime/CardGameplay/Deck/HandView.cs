487e03f90597614a26e279f27f5c89ad654200fb
GDIVX <yarin107@gmail.com>
2024-09-29 14:05:22 +0300

    Update HandView.cs

--- Assets/Scripts/Runtime/CardGameplay/Deck/HandView.cs
+++ Assets/Scripts/Runtime/CardGameplay/Deck/HandView.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using DG.Tweening;
+using Runtime.CardGameplay.Board;
 using Runtime.CardGameplay.Card;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -9,16 +10,16 @@ namespace Runtime.CardGameplay.Deck
 {
     public class HandView : HorizontalCardListView
     {
-
-
         private void Awake()
         {
             // Subscribe to events for card addition and removal if needed
             HandController.Instance.OnCardAdded += OnCardAdded;
             HandController.Instance.OnCardRemoved += OnCardRemoved;
+            BoardController.Instance.OnCardAdded += OnCardAddedToBoard;
         }
 
-
-       
+        private void OnCardAddedToBoard(CardController obj)
+        {
+        }
     }
 }
